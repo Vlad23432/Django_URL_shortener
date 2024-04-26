a7a3b33212591743c01b1c7a2e688e921fb1f83e
GreatRaksin <greatraksin@icloud.com>
2024-04-26 18:57:03 +0300

    Adding universal view function for pages

--- content/urls.py
+++ content/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-	path('<slug:url>/', views.page_view, name='page_detail')  # путь генерируется автоматически полем url из модели Page
+	path('<slug:url>/', views.page_view, name='page_detail'),  # путь генерируется автоматически полем url из модели Page
+	path('<slug:url>/<slug:child_url>/', views.page_view, name='page_nested'),
 ]
 
--- content/views.py
+++ content/views.py
@@ -3,6 +3,9 @@ from .models import Page
 
 
 # Create your views here.
-def page_view(request, url):
-	page = Page.objects.get(url=url)  # из базы данных достать объект страницы, где поле url совпадает с запрошенным url
+def page_view(request, url, child_url=None):
+	try:
+		page = Page.objects.get(url=child_url)  # из базы данных достать объект страницы, где поле url совпадает с запрошенным url
+	except:
+		page = Page.objects.get(url=url)
 	return render(request, 'content/page.html', {'page': page})
